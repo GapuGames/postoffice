257485b1ec3aa78e407afffe4aebae33f4f043d5
hahasasa <hahasasa@nate.com>
2015-06-17 17:26:12 -0700

    기본적인 구조 제작중.

---
+++ Assets/Scripts/StreetUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StreetUI : MonoBehaviour {
+
+
+	private float m_lastTouchX = 0;
+	public float m_accelSlide = 0.5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void OnBeginDrag()
+	{
+		m_lastTouchX = Input.mousePosition.x;
+	}
+
+	public void OnDrag()
+	{
+		float newTouchX = Input.mousePosition.x;
+		float slideDelta = newTouchX - m_lastTouchX;
+		m_lastTouchX = newTouchX;
+
+		GameObject cameraGO = GameObject.Find("Street/Camera");
+		cameraGO.transform.position -= Vector3.right * slideDelta;
+
+		Debug.Log(slideDelta);
+	}
+
+}
