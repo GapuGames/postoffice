8c62d87dd09071849d72b00530866086062da967
hahasasa <hahasasa@nate.com>
2015-06-19 15:27:41 -0700

    로고씬 추가. 기타 잡다한 정리.

---
+++ Assets/Scripts/LogoShower.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogoShower : MonoBehaviour
+{
+
+	void Start()
+	{
+		Invoke("ChangeScene", 3);
+	}
+
+	void ChangeScene()
+	{
+		Application.LoadLevel("Street");
+	}
+}
