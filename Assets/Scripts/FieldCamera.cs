ae3c70b5bca1bc04cee78b3a299e189dba917ce6
hahasasa <hahasasa@nate.com>
2015-08-12 14:52:08 +0900

    카메라 방식 테스트를 위한 임시 옵션 적용.

---
+++ Assets/Scripts/FieldCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldCamera : MonoBehaviour
+{
+
+	public GameObject m_target = null;
+	private void LateUpdate()
+	{
+		if (m_target == null) return;
+		Vector2 pos2 = m_target.transform.position;
+		transform.position = pos2;
+	}
+
+}
---
+++ Assets/Scripts/TestOption.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestOption : MonoBehaviour
+{
+	public GameObject  m_target = null;
+	public FieldCamera m_camera = null;
+
+	public void DettachCamera()
+	{
+		if (m_camera.m_target != null) m_camera.m_target = null;
+		else m_camera.m_target = m_target;
+	}
+}
