d78e0e03a8af148c9338a0a38ecb0f296d0cdb07
hahasasa <hahasasa@nate.com>
2015-08-27 19:11:46 +0900

    필드 위 객체들 자동 깊이 수정 해주는 스크립트 추가.

---
+++ Assets/Scripts/SortingNode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SortingNode : MonoBehaviour
+{
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Selection.activeGameObject == null) return;
+		foreach (Transform tf in Selection.transforms)
+		{
+			Resort(tf);
+		}
+	}
+
+	private void Resort(Transform tf)
+	{
+		Transform parent = tf.parent;
+		if (parent == null) return;
+		if (parent.GetComponent<SortingNode>() == null) return;
+		Vector3 pos = tf.position;
+		pos.z = pos.y;
+		tf.position = pos;
+	}
+}
