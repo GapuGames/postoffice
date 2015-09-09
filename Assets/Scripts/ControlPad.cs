9a63ec140aff6a72d33b9bac168b6301e76c7cac
hahasasa <hahasasa@nate.com>
2015-09-09 20:01:38 +0900

    컨트롤 패드 리펙토링.

--- Assets/Scripts/ControlPad.cs
+++ Assets/Scripts/ControlPad.cs
@@ -1,29 +1,65 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System;
 using System.Collections;
 
 public class ControlPad : MonoBehaviour
 {
 
-	[SerializeField] 
-	private MovePanel   m_movePanel  = null;
-	private UnityAction m_actionDown = null;
-	private UnityAction m_actionUp   = null;
+	[SerializeField] private Transform   m_ball        = null;
+	[SerializeField] private float       m_maxDistance = 25.0f;
+	[SerializeField] private PadMove     m_padMove     = new PadMove();
+	[SerializeField] private ActionDown  m_actionDown  = new ActionDown();
+	[SerializeField] private ActionUp    m_actionUp    = new ActionUp();
+	private Vector2    m_originPosition;
 
 	public void OnActionDown()
 	{
-		if (m_actionDown != null) m_actionDown();
+		m_actionDown.Invoke();
 	}
 	
 	public void OnActionUp()
 	{
-		if (m_actionUp != null) m_actionUp();
+		m_actionUp.Invoke();
 	}
 
 	public void SetControlCallback(UnityAction<Vector2, float> movingCallback, UnityAction actionDown, UnityAction actionUp)
 	{
-		if (m_movePanel != null) m_movePanel.m_movingCallback = movingCallback;
-		m_actionDown = actionDown;
-		m_actionUp   = actionUp;
+		m_padMove.AddListener(movingCallback);
+		m_actionDown.AddListener(actionUp);
+		m_actionUp.AddListener(actionUp);
 	}
+	
+	public void OnDrag(BaseEventData bed)
+	{
+		PointerEventData ped = bed as PointerEventData;
+		if (!ped.dragging) return;
+		if (!ped.IsPointerMoving()) return;
+		
+		Vector2 delta    = ped.position - m_originPosition;
+		Vector2 normal   = delta.normalized;
+		float   distance = delta.magnitude;
+		float   length   = (distance > m_maxDistance)? m_maxDistance : distance;
+		
+		m_ball.position  = m_originPosition + (normal * length);
+		
+		if (m_padMove != null) m_padMove.Invoke(normal, length / m_maxDistance);
+	}
+	
+	public void OnEndDrag()
+	{
+		m_ball.position = m_originPosition;
+		if (m_padMove != null) m_padMove.Invoke(Vector2.zero, 0);
+	}
+	
+	private void Start ()
+	{
+		m_originPosition = m_ball.position;
+	}
+	
+	[Serializable] public class PadMove    : UnityEvent<Vector2, float> {};
+	[Serializable] public class ActionUp   : UnityEvent {};
+	[Serializable] public class ActionDown : UnityEvent {};
 }
