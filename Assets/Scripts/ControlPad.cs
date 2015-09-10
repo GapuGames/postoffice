4a485a2cc83661084113dc4befbcf95fe88a6bfb
hahasasa <hahasasa@nate.com>
2015-09-10 23:07:55 +0900

    캐릭터 정보 무결성 유지를 위한 추가 에디터 기능 작업. 안쓰는 스크립트 제거. 캐릭터 조작 관련 스크립트 개선중.

--- Assets/Scripts/ControlPad.cs
+++ Assets/Scripts/ControlPad.cs
@@ -7,35 +7,28 @@ using System.Collections;
 
 public class ControlPad : MonoBehaviour
 {
-	
-	public void SetControlCallback(UnityAction<Vector2, float> movingCallback, UnityAction actionDown, UnityAction actionUp)
-	{
-		m_padMove.AddListener(movingCallback);
-		m_actionDown.AddListener(actionUp);
-		m_actionUp.AddListener(actionUp);
-	}
+	public PadMoveEvent     PadMove     = new PadMoveEvent();
+	public ActionDownEvent  ActionDown  = new ActionDownEvent();
+	public ActionUpEvent    ActionUp    = new ActionUpEvent();
 
-	//! private members, callback methods or anythings you don't need to worry about
+	//! private members, callbacks or anythings that you don't need to worry about
 	#region
-	[Serializable] public class PadMove    : UnityEvent<Vector2, float> {};
-	[Serializable] public class ActionUp   : UnityEvent {};
-	[Serializable] public class ActionDown : UnityEvent {};
+	[Serializable] public class PadMoveEvent    : UnityEvent<Vector2, float> {};
+	[Serializable] public class ActionUpEvent   : UnityEvent {};
+	[Serializable] public class ActionDownEvent : UnityEvent {};
 	
 	[SerializeField] private Transform   m_ball        = null;
 	[SerializeField] private float       m_maxDistance = 25.0f;
-	[SerializeField] private PadMove     m_padMove     = new PadMove();
-	[SerializeField] private ActionDown  m_actionDown  = new ActionDown();
-	[SerializeField] private ActionUp    m_actionUp    = new ActionUp();
 	private Vector2    m_originPosition;
 
 	public void OnActionDown()
 	{
-		m_actionDown.Invoke();
+		ActionDown.Invoke();
 	}
 	
 	public void OnActionUp()
 	{
-		m_actionUp.Invoke();
+		ActionUp.Invoke();
 	}
 	
 	public void OnDrag(BaseEventData bed)
@@ -51,13 +44,13 @@ public class ControlPad : MonoBehaviour
 		
 		m_ball.position  = m_originPosition + (normal * length);
 		
-		if (m_padMove != null) m_padMove.Invoke(normal, length / m_maxDistance);
+		if (PadMove != null) PadMove.Invoke(normal, length / m_maxDistance);
 	}
 	
 	public void OnEndDrag()
 	{
 		m_ball.position = m_originPosition;
-		if (m_padMove != null) m_padMove.Invoke(Vector2.zero, 0);
+		if (PadMove != null) PadMove.Invoke(Vector2.zero, 0);
 	}
 	
 	private void Start ()
